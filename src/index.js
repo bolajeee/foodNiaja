aa53fada72b6fed743075638d822de380e15eb69
Ibrahim Kolabalogun <115741084+bolajeee@users.noreply.github.com>
2023-01-09 15:24:06 +0100

    edit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const burger = document.querySelector("#burger");
 const menu = document.querySelector("#menu");
+// const learnMore = document.querySelectorAll(".loadMore");
+// const moreText = document.querySelectorAll(".moreText");
 
 burger.addEventListener("click",function(){
-    if(menu.classList.contains("hidden")){
-        menu.classList.remove("hidden");
+    if(menu.classList.contains("hidden", )){
+        menu.classList.remove("hidden", );
 
     } else{
-        menu.classList.add("hidden");
+        menu.classList.add("hidden", );
     }
 });
+
+// loadMore.addEventListener("click",() => {
+//     let loadMore = [ ];
+
+// for (let i = 0; i <= loadMore.length; learnMore++) {
+//        if (moreText.classList.contains("hidden")) {
+//          moreText.classList.remove("hidden");
+//        } else {
+//          moreText.classList.add("hidden");
+//        }
+// }
+// })
