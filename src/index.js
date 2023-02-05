d095f4ab051ecf86f91bb5caf7ebdf4e6127d9b7
Ibrahim Kolabalogun <115741084+bolajeee@users.noreply.github.com>
2023-02-05 12:54:31 +0100

    edit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ let slides = document.getElementById("descriptionText");
 let slidesR = document.getElementById("reviewAspect");
 let slideF = document.getElementById("reviewForm");
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
 
 burger.addEventListener("click",function(){
     if(menu.classList.contains("hidden", )){
@@ -29,7 +33,37 @@ review.addEventListener("click", () =>{
     slideF.style.display = "block";
 })
 
+function showSlides(n) {
+    let i;
+    let Slide = document.getElementById("mySlides");
+    if (n > Slide.length){
+        slideIndex = 1
+    }if(n < 1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+     slides[slideIndex - 1].style.display = "block";
+     dots[slideIndex - 1].className += " active";
+}
 
+let newArrivals = document.getElementById("newArrivals");
+let bestSales = document.getElementById("bestSales");
+let salesItems = document.getElementById("salesItems");
 
+newArrivals.addEventListener("click", () =>{
+    newArrivals.style.display ="block";
+    bestSales.style.display = "none";
+    salesItems.style.display = "none"
+})
 
+bestSales.addEventListener("click", ()=>{
+        newArrivals.style.display = "none";
+        bestSales.style.display = "block";
+        salesItems.style.display = "none";
+})
 
+salesItems.addEventListener("click", ()=>{
+       newArrivals.style.display = "none";
+        bestSales.style.display = "none";
+        salesItems.style.display = "block";
+})
